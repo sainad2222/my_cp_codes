43eea43af889e36a0cb02e85c24d224b8a557f9a
sainad <>
2020-05-03 23:24:00 +0530

    Add solution for problem `A - Football` Link: https://codeforces.com/contest/96/problem/A Tags: implementation, strings, *900 Ref: https://codeforces.com/contest/96/submission/78916537

--- codeforces/96/A.py
+++ codeforces/96/A.py
@@ -1,2 +1,11 @@
 s = input()
-print("YES") if('0000000' in s or '1111111' in s) else print("NO")
+flag=1
+for i in range(len(s)-1):
+    for j in range(i+1,len(s)):
+        if(s[i]==s[j]):
+            if(j-i>=6):
+                flag=0
+                break
+        else:
+            break
+print("YES") if flag==0 else print("NO")
