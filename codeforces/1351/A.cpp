a30f94a018e02e3ee7bb01caf34a0d515e9b0a47
sainad <>
2020-05-08 22:59:00 +0530

    Add solution for problem `A - A+B (Trial Problem)` Link: https://codeforces.com/contest/1351/problem/A Tags: implementation, *800 Ref: https://codeforces.com/contest/1351/submission/79402063

---
+++ codeforces/1351/A.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+
+#define w(t)            int t;cin>>t;while(t--)
+#define INT             long long
+#define mod             1000000007
+#define inf             (int)1e18
+#define endl            "\n"
+#define REP(i,a,b)      for(int i=a;i<=b;i++)
+#define FOR(i,n)          for(int i=0;i<n;i++)
+using namespace std;
+int main(){
+ios_base::sync_with_stdio(0);cin.tie(NULL);
+    w(t){
+    int a,b;
+    cin>>a>>b;
+    cout<<a+b<<endl;
+    }
+
+    return 0;
+    
+}
