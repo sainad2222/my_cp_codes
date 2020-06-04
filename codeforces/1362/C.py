1de39c07d358b134efcb00201044bf21cd702527
sainad <>
2020-06-04 23:57:00 +0530

    Add solution for problem `C - Johnny and Another Rating Drop` Link: https://codeforces.com/contest/1362/problem/C Tags: bitmasks, greedy, math, *1400 Ref: https://codeforces.com/contest/1362/submission/82567570

--- codeforces/1362/C.py
+++ codeforces/1362/C.py
@@ -1,10 +1,5 @@
-def  countSetBits(n): 
-    count = 0
-    while (n): 
-        count += n & 1
-        n>>= 1
-    return count
-
 for _ in range(int(input())):
-    n=int(input())
-    print(2*n-countSetBits(n))
+    n = int(input())
+    s = '{0:b}'.format(n)
+    setBits = s.count('1')
+    print(2*n-setBits)
