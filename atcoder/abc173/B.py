c0f14fb73f9894d5ffa61870bb2ceb0f995a4fdc
sainad <>
2020-07-05 17:36:00 +0530

    Add solution for problem `B - Judge Status Summary` Link: https://atcoder.jp/contests/abc173/tasks/abc173_b Tags: AtCoder, *200 Ref: https://atcoder.jp/contests/abc173/submissions/14974331

---
+++ atcoder/abc173/B.py
@@ -0,0 +1,16 @@
+ac,tle,wa,re=0,0,0,0
+n = int(input())
+for i in range(n):
+    s = input()
+    if s=="AC":
+        ac+=1
+    elif s=="WA":
+        wa+=1
+    elif s=="TLE":
+        tle+=1
+    else:
+        re+=1
+print("AC x",ac)
+print("WA x",wa)
+print("TLE x",tle)
+print("RE x",re)
