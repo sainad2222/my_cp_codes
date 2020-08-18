49e34aa7eaa15305d9a59adfa56b9700a14ffb81
sainad <>
2020-08-18 14:45:00 +0530

    Add solution for problem `A - Suborrays` Link: https://codeforces.com/contest/1391/problem/A Tags: constructive algorithms, math, *800 Ref: https://codeforces.com/contest/1391/submission/90293451

---
+++ codeforces/1391/A.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+
+#define w(t)            int t;cin>>t;while(t--)
+#define INT             long long
+#define mod             1000000007
+#define inf             (int)1e18
+#define endl            "\n"
+#define inpArr(vec)          for(auto &it:vec) cin>>it
+#define outArr(vec)          for(auto &it:vec) cout<<it<<" ";cout<<endl;
+using namespace std;
+void solve(){
+	int n;
+	cin>>n;
+	for (int i = n; i >= 1;i--)
+		cout << i << " ";
+	cout << endl;
+}
+
+
+
+
+int main(){
+ios_base::sync_with_stdio(0);cin.tie(NULL);
+
+w(t) {
+	solve();
+}
+
+	return 0;
+	
+}
