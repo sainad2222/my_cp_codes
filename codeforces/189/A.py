c5e0af0ac8fff5061576f24bffb206f5d9b58ed5
sainad <>
2020-09-13 13:59:00 +0530

    Add solution for problem `A - Cut Ribbon` Link: https://codeforces.com/contest/189/problem/A Tags: brute force, dp, *1300 Ref: https://codeforces.com/contest/189/submission/92695954

--- codeforces/189/A.py
+++ codeforces/189/A.py
@@ -1,37 +1,49 @@
-from types import GeneratorType
-def bootstrap(f, stack=[]):
-    def wrappedfunc(*args, **kwargs):
-        if stack:
-            return f(*args, **kwargs)
-        else:
-            to = f(*args, **kwargs)
-            while True:
-                if type(to) is GeneratorType:
-                    stack.append(to)
-                    to = next(to)
-                else:
-                    stack.pop()
-                    if not stack:
-                        break
-                    to = stack[-1].send(to)
-            return to
-    return wrappedfunc
-dpTD = {}
-@bootstrap
-def knapsackTD(wt,C):
-    n = len(wt)
-    if C in dpTD:
-        yield dpTD[C]
-    if C==0:
-        yield 0
-    ma = -float('inf')
-    for coin in wt:
-        if coin<=C:
-            tmp_max = yield knapsackTD(wt,C-coin)
-            if tmp_max + 1 > ma:
-                ma = tmp_max + 1
-    dpTD[C] = ma
-    yield dpTD[C]
-n, a, b, c = map(int, input().split())
-wt = [a, b, c]
-print(knapsackTD(wt,n))
+# import sys
+# sys.setrecursionlimit(10**6)
+# n, a, b, c = map(int, input().split())
+# wt = [a, b, c]
+# dp = {}
+# def knapsack(wt,C):
+#     n = len(wt)
+#     if C in dp:
+#         return dp[C]
+#     if C == 0:
+#         return 0
+#     ans = -1
+#     for val in wt:
+#         if val <= C:
+#             ans = max(ans, 1 + knapsack(wt, C - val))
+#     dp[C] = ans
+#     return ans
+# print(knapsack(wt,n))
+import sys 
+    
+#As Python has recursion limit we extend the recusrion limit to avoid runtime error due to large number of recursive calls 
+sys.setrecursionlimit(500000000) 
+    
+def f(n): 
+    """ 
+    returns maximum number of pieces that can be cut for ribbon with length n 
+    """ 
+        
+    # If maximum num of pieces for length n is already computed simply return it 
+    if n in memo.keys(): 
+        return memo[n] 
+    
+    ans = float("-inf") 
+    if n == 0: 
+        return 0 
+    for length in l: 
+        # Cut into pieces if only we dont have negative length of ribbon 
+        if n >= length: 
+            ans = max(ans, 1 + f(n - length)) 
+    #Cache the result 
+    memo[n] = ans 
+    return ans 
+        
+#Dict to store computed values 
+memo = dict() 
+    
+l = list(map(int, input().split())) 
+n, l = l[0], l[1:] 
+print(f(n)) 
