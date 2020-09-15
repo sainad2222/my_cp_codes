d1f5f33a365f0de2bec632c50c69d4c437614211
sainad <>
2020-09-15 22:33:00 +0530

    Add solution for problem `A - Frog 1` Link: https://atcoder.jp/contests/dp/tasks/dp_a Tags: AtCoder, *100 Ref: https://atcoder.jp/contests/dp/submissions/16772753

--- atcoder/dp/A.py
+++ atcoder/dp/A.py
@@ -1,23 +1,36 @@
+from types import GeneratorType
+def bootstrap(f, stack=[]):
+    def wrappedfunc(*args, **kwargs):
+        if stack:
+            return f(*args, **kwargs)
+        else:
+            to = f(*args, **kwargs)
+            while True:
+                if type(to) is GeneratorType:
+                    stack.append(to)
+                    to = next(to)
+                else:
+                    stack.pop()
+                    if not stack:
+                        break
+                    to = stack[-1].send(to)
+            return to
+    return wrappedfunc
 n = int(input())
-lis = list(map(int,input().split()))
-# dp = {}
+lis = list(map(int, input().split()))
+dp = {}
+@bootstrap
 def TD(i):
-    if i in dp:
-        return dp[i]
-    if i == n-1:
-        return 0
-    mi = float('inf')
-    if (i < n - 1):
-        mi = min(mi,abs(lis[i + 1] - lis[i]) + TD(i + 1))
-    if i < n - 2:
-        mi = min(mi,abs(lis[i + 2] - lis[i]) + TD(i + 2))
-    dp[i] = mi
-    return mi
-# print(TD(0))
+    if i == n - 1:
+        yield 0
 
-dp = [0 for i in range(n)]
-dp[0] = 0
-dp[1] = abs(lis[1] - lis[0])
-for i in range(2, n):
-    dp[i] = min(abs(lis[i] - lis[i - 1]) + dp[i - 1], abs(lis[i] - lis[i - 2]) + dp[i - 2])
-print(dp[n-1])
+    if i in dp:
+        yield dp[i]
+    mi = float('inf')
+    for j in range(1, 3):
+        if i + j < n:
+            tmp = yield TD(i+j)
+            mi = min(mi, abs(lis[i] - lis[i+j])+ tmp)
+    dp[i] = mi
+    yield mi
+print(TD(0))   
