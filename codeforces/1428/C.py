c02a08eda403e4ea6f15dc44160e18a963edc12d
sainad <>
2020-10-17 21:54:00 +0530

    Add solution for problem `C - ABBB` Link: https://codeforces.com/contest/1428/problem/C Tags: brute force, data structures, greedy, strings, *1100 Ref: https://codeforces.com/contest/1428/submission/95810425

--- codeforces/1428/C.py
+++ codeforces/1428/C.py
@@ -1,11 +1,9 @@
 for _ in range(int(input())):
-    s = input()
-    stack = 0
-    prev = ''
-    for char in s:
-        if stack and char == 'B':
-            stack -= 1
-        else:
-            stack += 1
-            prev = char
-    print(stack)
+	s = input()
+	stack = []
+	for char in s:
+		if stack and char == "B" and (stack[-1] == "A" or stack[-1] == "B"):
+			stack.pop()
+		else:
+			stack.append(char)
+	print(len(stack))
