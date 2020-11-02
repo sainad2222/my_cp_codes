5d25b859f9384bbbc72283688950e46d6d4b7bcb
sainad <>
2020-11-02 16:00:00 +0530

    Add solution for problem `D - Hachi` Link: https://atcoder.jp/contests/abc181/tasks/abc181_d Tags: AtCoder, *400 Ref: https://atcoder.jp/contests/abc181/submissions/17839544

--- atcoder/abc181/D.py
+++ atcoder/abc181/D.py
@@ -4,16 +4,15 @@ from collections import Counter
 
 def main():
     s = input()
-    ds = Counter(s)  # Counter of all chars
+    ds = Counter(s)
     if len(s) <= 2:
-        if int(s) % 8 == 0 or int(
-                s[::-1]) % 8 == 0:  # corner case because we can rearrange
+        if int(s) % 8 == 0 or int(s[::-1]) % 8 == 0:
             print("Yes")
         else:
             print("No")
         return
-    for i in range(104, 1001, 8):  # Iterating over all 3 digit multiples of 8
-        counter = Counter(str(i))  # comparing counters
+    for i in range(104, 1001, 8):
+        counter = Counter(str(i))
         flag = 1
         for i in counter:
             if ds[i] < counter[i]:
