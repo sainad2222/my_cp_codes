e2b5d005616f3bb62c7c5f22b96e7267a1b9e665
sainad <>
2020-12-11 21:11:00 +0530

    Add solution for problem `B - Find the Spruce` Link: https://codeforces.com/contest/1461/problem/B Tags: brute force, dp, implementation, *1400 Ref: https://codeforces.com/contest/1461/submission/100941383

---
+++ codeforces/1461/B.py
@@ -0,0 +1,155 @@
+from __future__ import division, print_function
+
+import os
+import sys
+from io import BytesIO, IOBase
+
+def main():
+    def precomp():
+        fin = []
+        for i in range(n):
+            cur = 0
+            tmp = []
+            j = 0
+            while j<m:
+                if matrix[i][j]=='*':
+                    cur+=1
+                tmp.append(cur)
+                j+=1
+            fin.append(tmp)
+        return fin
+    def check(i,j,spuce):
+        if j+spuce>m:
+            return False
+        if matrix[i][j] != '*':
+            return False
+        return fin[i][j+spuce-1]-fin[i][j]+1 == spuce
+    def godeep(i,j):
+        ans = 0
+        spuce = 1
+        while i<n and j>=0:
+            if check(i,j,spuce):
+                ans+=1
+            else:
+                return ans
+            i+=1
+            j-=1
+            spuce+=2
+        return ans
+    for _ in range(int(input())):
+        n,m = map(int,input().split())
+        matrix = []
+        ans = 0
+        for i in range(n):
+            s = list(input())
+            matrix.append(s)
+        fin = precomp()
+        for i in range(n):
+            for j in range(m):
+                if matrix[i][j]=='*':
+                    #  print(godeep(i,j),i,j)
+                    ans += godeep(i,j)
+        print(ans)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if sys.version_info[0] < 3:
+    from __builtin__ import xrange as range
+    from future_builtins import ascii, filter, hex, map, oct, zip
+
+# region fastio
+# Credits
+# # template credits to cheran-senthil's github Repo
+
+BUFSIZE = 8192
+
+
+class FastIO(IOBase):
+    newlines = 0
+
+    def __init__(self, file):
+        self._fd = file.fileno()
+        self.buffer = BytesIO()
+        self.writable = "x" in file.mode or "r" not in file.mode
+        self.write = self.buffer.write if self.writable else None
+
+    def read(self):
+        while True:
+            b = os.read(self._fd, max(os.fstat(self._fd).st_size, BUFSIZE))
+            if not b:
+                break
+            ptr = self.buffer.tell()
+            self.buffer.seek(0, 2), self.buffer.write(b), self.buffer.seek(ptr)
+        self.newlines = 0
+        return self.buffer.read()
+
+    def readline(self):
+        while self.newlines == 0:
+            b = os.read(self._fd, max(os.fstat(self._fd).st_size, BUFSIZE))
+            self.newlines = b.count(b"\n") + (not b)
+            ptr = self.buffer.tell()
+            self.buffer.seek(0, 2), self.buffer.write(b), self.buffer.seek(ptr)
+        self.newlines -= 1
+        return self.buffer.readline()
+
+    def flush(self):
+        if self.writable:
+            os.write(self._fd, self.buffer.getvalue())
+            self.buffer.truncate(0), self.buffer.seek(0)
+
+
+class IOWrapper(IOBase):
+    def __init__(self, file):
+        self.buffer = FastIO(file)
+        self.flush = self.buffer.flush
+        self.writable = self.buffer.writable
+        self.write = lambda s: self.buffer.write(s.encode("ascii"))
+        self.read = lambda: self.buffer.read().decode("ascii")
+        self.readline = lambda: self.buffer.readline().decode("ascii")
+
+
+def print(*args, **kwargs):
+    """Prints the values to a stream, or to sys.stdout by default."""
+    sep, file = kwargs.pop("sep", " "), kwargs.pop("file", sys.stdout)
+    at_start = True
+    for x in args:
+        if not at_start:
+            file.write(sep)
+        file.write(str(x))
+        at_start = False
+    file.write(kwargs.pop("end", "\n"))
+    if kwargs.pop("flush", False):
+        file.flush()
+
+
+if sys.version_info[0] < 3:
+    sys.stdin, sys.stdout = FastIO(sys.stdin), FastIO(sys.stdout)
+else:
+    sys.stdin, sys.stdout = IOWrapper(sys.stdin), IOWrapper(sys.stdout)
+
+input = lambda: sys.stdin.readline().rstrip("\r\n")
+
+# endregion
+
+
+if __name__ == "__main__":
+    main()
