cde4a5efba3e89fc846f8321060ae3d5cbb845c6
sainad <>
2020-12-17 22:28:00 +0530

    Add solution for problem `B - Find The Array` Link: https://codeforces.com/contest/1463/problem/B Tags: bitmasks, constructive algorithms, greedy, *1400 Ref: https://codeforces.com/contest/1463/submission/101582004

--- codeforces/1463/B.cpp
+++ codeforces/1463/B.cpp
@@ -1,57 +1,52 @@
-// NTFS: aaryamannx in stevenkplus twitch
-// Idea is to replace odd or even position elements with 1 whichever sum is lower
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
-#define INT long long
-#define endl "\n"
-void solve()
-{
+#define INT             long long
+#define endl            "\n"
+#define inpArr(vec)          for(auto &it:vec) cin>>it;
+#define outArr(vec)          for(auto &it:vec) cout<<it<<" ";cout<<endl;
+void solve(){
     int n;
-    cin >> n;
+    cin>>n;
     vector<int> lis(n);
     INT total = 0;
     INT odd = 0;
     bool od = true;
-    for (auto& it : lis) {
-        cin >> it;
-        total += it;
-        if (od)
-            odd += it;
+    for(auto &it:lis){
+        cin>>it;
+        total+=it;
+        if(od) odd+=it;
         od = !od;
     }
-    INT even = total - odd;
-    if (odd < even) {
+    INT even = total-odd;
+    if(odd<even){
         od = true;
-        for (auto& it : lis) {
-            if (od)
-                cout << 1 << " ";
-            else
-                cout << it << " ";
+        for(auto &it:lis){
+            if(od) cout<<1<<" ";
+            else cout<<it<<" ";
             od = !od;
         }
-        cout << endl;
-    } else {
+        cout<<endl;
+    }
+    else{
         od = false;
-        for (auto& it : lis) {
-            if (od)
-                cout << 1 << " ";
-            else
-                cout << it << " ";
+        for(auto &it:lis){
+            if(od) cout<<1<<" ";
+            else cout<<it<<" ";
             od = !od;
         }
-        cout << endl;
+        cout<<endl;
     }
 }
 
-int main()
-{
-    ios_base::sync_with_stdio(0);
-    cin.tie(NULL);
-    int tt = 1;
-    cin >> tt;
-    while (tt--) {
-        solve();
-    }
 
-    return 0;
+int main(){
+ios_base::sync_with_stdio(0);cin.tie(NULL);
+int tt=1;
+cin >> tt;
+while(tt--){
+    solve();
+}
+
+return 0;
+
 }
