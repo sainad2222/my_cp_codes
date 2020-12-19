754da8a1899557e3b6bf1e9aa184279438d6faac
sainad <>
2020-12-19 22:45:00 +0530

    Add solution for problem `C - Row GCD` Link: https://codeforces.com/contest/1459/problem/C Tags: math, number theory, *1600 Ref: https://codeforces.com/contest/1459/submission/101791357

--- codeforces/1459/C.cpp
+++ codeforces/1459/C.cpp
@@ -1,7 +1,7 @@
 // NTFS: Editorial
 // gcd(a,b) = gcd(a-b,b) euclidean gcd I guess
 // now this holds for multiple arguments too
-// so gcd(a1+bj,...,an+bj) = gcd(a1+bj,a2-a1,a3-a1,...) after subtracting a1+bj from all other elements except first
+// so gcd(a1+bj,...,an+bj) = gcd(a1+bj,a2-a1,a3-a2,...) after subtracting a1+bj from all other elements except first
 // so for every j only first element is changing
 #include <bits/stdc++.h>
 using namespace std;
@@ -29,8 +29,8 @@ void solve()
     for (int i = 1; i < n; i++) {
         g = __gcd(g, a[i] - a[0]);
     }
-    for (int i = 0; i < m; i++) {                  // also adding to a[0] only is confusing too
-        cout << abs(__gcd(g, a[0] + b[i])) << " "; // abs part is confusing
+    for (int i = 0; i < m; i++) {
+        cout << abs(__gcd(g, a[0] + b[i])) << " ";
     }
     cout << endl;
 }
